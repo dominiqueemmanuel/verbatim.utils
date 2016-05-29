18c21901cc6ffc4f65709bf9ac0b6b195e78f581
dominiqueemmanuel <dominique_emmanuel@hotmail.com>
2016-05-29 16:01:59 +0200

    Ajout d'un traitement spécial pour les mots avec majuscules

--- R/spell_correction.R
+++ R/spell_correction.R
@@ -15,7 +15,10 @@
 ####################################################
 
 #' @export spell_correction
-spell_correction <- function(txt, lang="en", minimal_occurence = 3, excluded_word = NULL) {
+spell_correction <- function(txt, lang="en"
+                             , minimal_occurence = 3
+                             , excluded_word = NULL
+                             , ingore_cap_word = TRUE) {
   lang <- match.arg(tolower(lang), c("en", "fr"))
   dict <- paste0(lang,"_",if(lang=="en") "US" else toupper(lang))
   library(magrittr)
@@ -35,21 +38,41 @@ spell_correction <- function(txt, lang="en", minimal_occurence = 3, excluded_wor
 
   ## Pour les autres signes de ponctuation on rajoute un espace avant et après
   p<-'\\!|\\"|\\$|\\%|\\(|\\)|\\*|\\+|\\[+]|\\]|\\,|\\.|\\/|\\:|\\;|\\<|\\=|\\>|\\?|\\\\|\\`|\\{|\\||\\}'%>%force_encoding
-
   txt <- gsub(paste0("(",p,")")," \\1 ",txt, perl=TRUE)
 
+  # Les premiers mots de phrases sont mis en minuscules pour ne pas intégréer le cas des majsucules en début de phrases
+  if(ingore_cap_word){
+    txt<-gsubfn::gsubfn("(?:^|(?:[:.!?]\\s))+(\\w+)",replacement=function(x,y,z)paste0(tolower(x),z,collapse=""),backref=2,x=txt)
+  }
 
   ## On créer un séparateur artificiel de verbatim pour pouvoir traiter les verbatims en une fois (=> optimisation des temps de calculs)
   txt <- paste(txt,collapse=" _SEPARATEUR_DE_VERBATIM_ ")
   txt <- gsub(" +"," ",txt)
   txt <- str_split(txt," ")[[1]]
+
+
+  #on prend chaque mot hors séparateur artificiel
   id<-which(txt!="_SEPARATEUR_DE_VERBATIM_" & nchar(txt)>1)
   x<-txt[id]
+
+  # le fait de travailler sur les levels des facteurs vas accélérer les traitementes (éviter les doublons d'analyses)
   x<-factor(x)
   l<-levels(x)
+
+  # On ne travaille que sur les mots aillant au plus minimal_occurence occurence
   n<-table(x)
   n<-names(n[n<=minimal_occurence])
-  n<-setdiff(n,excluded_word%>%force_encoding)
+
+  # On retire les mots à ne pas analyser (excluded_word)
+  # n<-setdiff(n,excluded_word%>%force_encoding)
+  n2<-as.vector(sapply(excluded_word,function(t)which(grepl(paste0("\\b",t,"\\b"),n,ignore.case = TRUE))))
+  if(length(n2)>0)n<-n[-n2]
+
+  # On retire les mots qui ont une majuscule
+  if(ingore_cap_word){
+    n <- n[!grepl("[A-Z]",n)]
+  }
+
   y<-hunspell_check(n,dict=dict)
   q<-!grepl("[^\\'\\-[:^punct:]]"%>%force_encoding,n,perl=TRUE)
   e<-hunspell_suggest(n[!y & q],dict = dict)%>%sapply(function(t)t[1])
@@ -64,4 +87,4 @@ spell_correction <- function(txt, lang="en", minimal_occurence = 3, excluded_wor
 }
 
 
-# spell_correction(c("une Phrase avec une fauute","Une phrase avec un mot inconnu xxsf"), lang="fr")
+# spell_correction(c("une Phrase avec une fauute et une autre Fauute","Une phrase avec un mot inconnu xxsf"), lang="fr")
