581794f8a49dfe7bcfe8747b3cd3a6a82fee58f8
DANBA <danba.abdelhadi@gmail.com>
2016-06-18 17:47:59 +0200

    Lemmitiseur

--- R/lemmatisation.R
+++ R/lemmatisation.R
@@ -16,10 +16,63 @@
 ####################################################
 
 
+#
+chunk2 <- function(x,n){
+  if(n>1)
+    return(split(x, cut(seq_along(x), n, labels = FALSE)))
+  else
+    return(x)
+}
+
+sub_lemmatisation <- function(txt, lang="fr"){
+
+  ## Nom de fichier source
+  testtxt_in_name <- tempfile()
+  ## Le fichier source
+  testtxt_in<-file(testtxt_in_name,encoding="UTF-8")
+  writeLines(txt,testtxt_in)
+  close(testtxt_in)
+
+  ## Nom de fichier en sortie
+  testtxt_out_name <- tempfile()
+  ## Le fichier en sortie
+  testtxt_out<-file(testtxt_out_name,encoding="UTF-8")
+
+  ## Écriture de la commande
+  command <-   paste0("analyze -f ",lang,".cfg <",testtxt_in_name," >",testtxt_out_name)
+  ## Exécution de la commande
+  res <- system(command)
+
+  ## s'il y a eu un problème on renvoi NULL
+  if(res!=0)return(NULL)
+
+  ## On lit le fichier en sortie
+  out<-readLines(testtxt_out_name,encoding="UTF-8")
+  print(out)
+  out <- out[-length(out)]
+
+  out <- str_split(out," ")
+  print(out)
+  out_lemme <- sapply(out,function(t)t[[2]])
+  out_categ <- sapply(out,function(t)t[[3]])
+  out_categ[out_lemme==tolower("MOT_SEPARATEUR_DE_VERBATIM")] <- tolower("MOT_SEPARATEUR_DE_VERBATIM")
+
+  out_lemme <- str_trim(str_split(paste(out_lemme,collapse = " "), tolower("MOT_SEPARATEUR_DE_VERBATIM"))[[1]])
+  out_lemme <- out_lemme[-length(out_lemme)]
+
+  out_categ <- str_trim(str_split(paste(out_categ,collapse = " "), tolower("MOT_SEPARATEUR_DE_VERBATIM"))[[1]])
+  out_categ <- out_categ[-length(out_categ)]
+
+  return(list(txt_lemme=out_lemme,txt_categ=out_categ))
+
+}
+
+
 #' @export lemmatisation
 lemmatisation  <- function(txt, lang="en", mc.cores = 4) {
   lang <- match.arg(tolower(lang), c("en", "fr"))
 
+  library(stringr)
   library(magrittr)
   ## Need Freeling to be installed
 
@@ -29,31 +82,31 @@ lemmatisation  <- function(txt, lang="en", mc.cores = 4) {
     mc.cores <- 1
   }
 
-
-
   ## Méthod pour consever l'information de séparateur de verbatims
-  txt <- paste0(txt,collapse=" _MOT_SEPARATEUR_DE_VERBATIM_ ")
-
-
-
   ##### LE CODE FREELING #####################
-  txt<-str_split(txt," ")%>%unlist
-  txt <- paste(txt,ifelse(txt=="_MOT_SEPARATEUR_DE_VERBATIM_",txt,substr(txt,1,5)),"NCFS000",runif(length(txt)))
-  ##### FIN LE CODE FREELING ###########################
 
+  # Mapping
+  txt <- lapply(chunk2(txt, mc.cores), function(lis_sent) sapply(lis_sent, function(sent) paste0(sent," MOT_SEPARATEUR_DE_VERBATIM")))
+  txt <- mclapply(txt, function(x) sub_lemmatisation(x,lang=lang), mc.cores = mc.cores)
 
+  # Reducing
+  result = list()
+  for(e in 1:length(txt)){
+    result$txt_lemme <- c(result$txt_lemme, txt[[e]]$txt_lemme)
+    result$txt_categ <- c(result$txt_categ, txt[[e]]$txt_categ)
+  }
 
-
-  txt <- str_split(txt," ")
-  txt_lemme <- sapply(txt,function(t)t[[2]])
-  txt_categ <- sapply(txt,function(t)t[[3]])
-  txt_categ[txt_lemme=="_MOT_SEPARATEUR_DE_VERBATIM_"] <- "_MOT_SEPARATEUR_DE_VERBATIM_"
-
-  txt_lemme <- str_trim(str_split(paste(txt_lemme,collapse = " "),"_MOT_SEPARATEUR_DE_VERBATIM_")[[1]])
-  txt_categ <- str_trim(str_split(paste(txt_categ,collapse = " "),"_MOT_SEPARATEUR_DE_VERBATIM_")[[1]])
-
-
-  return(list(txt_lemme=txt_lemme,txt_categ=txt_categ))
-
-
+  return(result)
 }
+
+
+lemmatisation(c("kaeaj lamps kae","aleakk amloer al lamaiosn adar,","alors un deusieme text pour un exemple complet"))
+
+
+
+
+
+
+
+
+
