6f33f342c4e8686df9f21c99c2c5f3065ab6651e
dominiqueemmanuel <dominique_emmanuel@hotmail.com>
2016-12-31 13:08:48 +0100

    paste collapse

--- R/lemmatisation.R
+++ R/lemmatisation.R
@@ -170,6 +170,9 @@ library(stringi)
   out_categ <- str_trim(str_split(paste(out_categ,collapse = " "), tolower("MOT_SEPARATEUR_DE_VERBATIM"))[[1]])
   # out_categ <- out_categ[-length(out_categ)]
 
+
+  out_in <- str_trim(str_split(paste(out_in,collapse = " "), tolower("MOT_SEPARATEUR_DE_VERBATIM"))[[1]])
+
   return(list(txt_lemme=out_lemme,txt_categ=out_categ,txt_in=out_in))
 
 }
