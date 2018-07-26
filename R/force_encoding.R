4df71265623b8b3a8d3874cc08a2e57ffa88a0d8
Dominique EMMANUEL <dominique_emmanuel@hotmail.com>
2018-07-26 17:56:48 +0200

    Update force_encoding.R
    back to Ruchardet::detectEncoding

--- R/force_encoding.R
+++ R/force_encoding.R
@@ -13,9 +13,9 @@ force_encoding0 <- function(txt,target_encoding = "UTF-8",tolower=FALSE,trace=FA
 {
   if(length(txt)==0)return(txt)
   if(!tolower){
-    txt <- vapply(txt,function(x){tryCatch({x<-iconv(x,rvest::guess_encoding(x)$encoding[1],target_encoding);x},error=function(e){if(trace){print(e);print(x)};x}) }, FUN.VALUE = character(1),USE.NAMES = FALSE)
+    txt <- vapply(txt,function(x){tryCatch({x<-iconv(x,Ruchardet::detectEncoding(x),target_encoding);x},error=function(e){if(trace){print(e);print(x)};x}) }, FUN.VALUE = character(1),USE.NAMES = FALSE)
   } else {
-    txt <- vapply(txt,function(x){tryCatch({x<-tolower(iconv(x,rvest::guess_encoding(x)$encoding[1],target_encoding));x},error=function(e){if(trace){print(e);print(x)};x})}, FUN.VALUE = character(1),USE.NAMES = FALSE)
+    txt <- vapply(txt,function(x){tryCatch({x<-tolower(iconv(x,Ruchardet::detectEncoding(x),target_encoding));x},error=function(e){if(trace){print(e);print(x)};x})}, FUN.VALUE = character(1),USE.NAMES = FALSE)
   }
   Encoding(txt) <- target_encoding
   return(txt)
